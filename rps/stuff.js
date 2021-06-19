5e9baaf4b65114d69454e722f37b0d7cd9aea0a4
Daniel <degtrdg@github.io>
2021-06-19 13:20:29 -0500

    Finished rock, paper, scissors program

--- rps/stuff.js
+++ rps/stuff.js
@@ -1,29 +1,34 @@
-	const compVars = ["r","p","s"];
-	document.onkeyup(function(e) {
-		let user = e.key;
-		let computerGuess = compVars[floor(3*(Math.random()))];
-		let wins=0;
-		let losses=0;
-
-		if((user === "r") ||(user === "s")||(user === "p")){
-			if(user == computerGuess){
-
-		}else if(user == "r" && computerGuess == "p"){
-
-		}else if(user == "r" && computerGuess == "s"){
-
-		}else if(user == "p" && computerGuess == "r"){
-
-		}else if(user == "p" && computerGuess == "s"){
-
-		}else if(user == "s" && computerGuess == "r"){
-
-		}else if(user == "s" && computerGuess == "p"){
-
-		}
-	    "<p>You chose: "+user+"</p>"+
-	    "<p>The computer chose: " +computerGuess+ "</p>"+
-	    "<p>wins: "+ wins +"</p>"+
-	    "<p>losses: " losses + "</p>"
-		} 
-	})
+const compVars = ["r", "p", "s"];
+const user = document.getElementById("user");
+const computer = document.getElementById("computer");
+const winsel = document.getElementById("wins");
+const lossesel = document.getElementById("losses");
+const total = document.getElementById("total");
+let wins = 0;
+let losses = 0;
+document.onkeyup = function (e) {
+  let userGuess = e.key;
+  let computerGuess = compVars[Math.floor(3 * Math.random())];
+  if (userGuess === "r" || userGuess === "s" || userGuess === "p") {
+    if (userGuess == computerGuess) {
+    } else if (userGuess == "r" && computerGuess == "p") {
+      losses++;
+    } else if (userGuess == "r" && computerGuess == "s") {
+      wins++;
+    } else if (userGuess == "p" && computerGuess == "r") {
+      wins++;
+    } else if (userGuess == "p" && computerGuess == "s") {
+      losses++;
+    } else if (userGuess == "s" && computerGuess == "r") {
+      losses++;
+    } else if (userGuess == "s" && computerGuess == "p") {
+      wins++;
+    }
+    user.textContent = "You played: " + userGuess;
+    computer.textContent = "The computer played: " + computerGuess;
+    winsel.textContent = "Your wins: " + wins;
+    lossesel.textContent = "Your losses: " + losses;
+    let t = wins + losses;
+    total.textContent = "Total games: " + t;
+  }
+};
